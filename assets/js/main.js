6906a3f6978fe0cf96bda7efc8110a73d997c5b6
dewisme cindy <dewismecindy@MacBook-Pro-de-dewisme.local>
2022-12-08 20:03:15 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("page chargée");
+
+  /*   document.addEventListener("click", () => {
+    console.log("clicked");
+  }); */
+
+  console.log("button>>>"), document.querySelector("#hide");
+
+  /*     .addEventListener("click", () => console.log("click on button"));
+  document.querySelector("p").classList.add("hidden"); */
+});
